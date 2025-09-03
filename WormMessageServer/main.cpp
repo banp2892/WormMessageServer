4e34f0df74d6bc225aef152f45be6244131ee374
banp2892 <sseva2892@gmail.com>
2025-09-03 20:28:58 +0300

    Изменил кодировку на UTF-8

---
+++ WormMessageServer/main.cpp
@@ -0,0 +1,14 @@
+﻿#include "TcpServer.h"
+#include<locale.h>  
+#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+
+	TcpServer Serv1(12345);
+	Serv1.start();
+	
+	std::cout<<Serv1.receiveData()<<std::endl;
+
+	Serv1.stop();
+}
